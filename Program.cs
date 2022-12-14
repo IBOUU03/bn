56ff31c49ed54e479d8fe3337e457d91a59268df
IBOUU03 <118839500+IBOUU03@users.noreply.github.com>
2022-12-14 17:24:34 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,83 +1,15 @@
-﻿Console.WriteLine("1. Gülləni tək tək at");
-Console.WriteLine("2. Güllənin hamısını birdən at");
-Console.WriteLine("3. Mərmini yenidən doldur");
-Console.WriteLine("4. Oyundan çıx");
-Console.WriteLine("5. Mərmi sayını gösdər");
-int bullet = 30;
-
-while (true)
+string test = Console.ReadLine();
+string newtest ="";
+for (int i = test.Length-1; i >= 0; i--)
 {
-    string number = Console.ReadLine();
+    newtest += test[i].ToString();
 
-    while (number == "")
-    {
-        Console.WriteLine("Duzgun reqem daxil edin");
-        string NewNumber = Console.ReadLine();
-        number = NewNumber;
-    }
-
-    int num = int.Parse(number);
-
-    CheckNum(num);
-    
-
-    if (num == 1)
-    {
-        
-        if (bullet > 0)
-        {   bullet--;
-            Console.WriteLine("Gulle atildi");
-        }
-        else 
-        {
-            Console.WriteLine("3 bassaraq gulle doldur");
-        }
-        
-
-    }
-    else if(num == 2)
-    {
-        if (bullet > 0)
-        {
-            bullet = 0;
-            Console.WriteLine("Gulle hamisi atildi");
-        }
-        else
-        {
-            Console.WriteLine("3 bassaraq gulle doldur");
-        }
-    }
-    else if (num == 3)
-    {
-        bullet = 30;
-        Console.WriteLine("Gulle dolduruldu");
-    }
-    else if (num == 4)
-    {
-        Console.WriteLine("Oyundan cixildi");
-        return;
-    }
-    else if (num == 5)
-    {
-        Console.WriteLine(bullet + " gulleniz var");
-    }   
 }
 
-    
-    static void CheckNum(int check)
-    {
+string[] test1;
+test1 = newtest.Split(' ');
 
-        while (check < 1 || check > 5)
-        {
-            Console.WriteLine("Duzgun reqem daxil edin");
-            int NewNum = int.Parse(Console.ReadLine());
-            check = NewNum;
-        };
-    }   
-    
-    
-    
-    
-    
-    
-    
+for (int i = test1.Length-1; i >= 0; i--)
+{
+    Console.Write(test1[i]+" ");
+}
